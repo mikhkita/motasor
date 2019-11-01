a9d1ed619ad7e47505fa16ccfcb7c8e1f3ba27a9
Чирцов Роман <rom4es_1993@mail.ru>
2019-11-01 15:06:56 +0700

    123

--- footer.php
+++ footer.php
@@ -343,11 +343,9 @@
 	</div>
 	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
 	<script type="text/javascript" src="js/jquery.fancybox.min.js"></script>
-	<!-- <script type="text/javascript" src="//maps.google.com/maps/api/js?sensor=false&key=AIzaSyD6Sy5r7sWQAelSn-4mu2JtVptFkEQ03YI"></script> -->
 	<script type="text/javascript" src="js/jquery.touch.min.js"></script>
 	<script type="text/javascript" src="js/jquery.maskedinput.min.js"></script>
 	<script type="text/javascript" src="js/jquery.validate.min.js"></script>
-	<script type="text/javascript" src="js/KitAnimate.min.js"></script>
 	<script type="text/javascript" src="js/slick.min.js"></script>
 	<script type="text/javascript" src="js/imask.min.js"></script>
 	<script type="text/javascript" src="js/KitSend.min.js"></script>
