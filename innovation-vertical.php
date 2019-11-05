1187c3c7a51cd092e764a417c68cf187fd138b96
Дмитрий Муленок <mulenokd@gmail.com>
2019-11-05 23:14:44 +0700

    no message

--- innovation-vertical.php
+++ innovation-vertical.php
@@ -11,13 +11,11 @@ $params = array(
 
 $innerText = 'Лидерство в атомной энергетике во многом зависит от интеллектуального и технологического превосходства. Ежегодно Росатом наращивает объемы заказа на научно-исследовательские и опытно-конструкторские работы, увеличивает финансирование на эти цели, поэтому подход отраслевых заказчиков к формированию НИОКР должен соответствовать тенденциям времени и профессиональному уровню передовых международных практик. Чтобы увеличить эффективность заказа на НИОКР и повысить «прозрачность» формирования программ исследований и разработок Блок по управлению инновациями Госкорпорации «Росатом» реализует ряд мероприятий в области управления НИОКР.';
 
-$title = "Инновационная вертикаль";
 include 'header.php';
 
 ?>
 
 	<div class="b-block b-block-inner">
-<<<<<<< HEAD
 		<div class="b-content-inner-wrap">
 			<ul class="b-nav">
 				<li class="active">
@@ -44,122 +42,60 @@ include 'header.php';
 				<div class="b-text">
 					<h1>Инновационная вертикаль</h1>
 					<p>Процесс консолидации инновационной деятельности обусловлен задачей достижения технологического лидерства отрасли и подкреплен решением руководства Росатома о построении функциональных вертикалей, которые бы пронизывали все дивизионы. Принцип дивизионального построения корпорации с функциональной вертикалью уже реализован внутри Госкорпорации в сферах управления финансами, кадрами, безопасностью, внутреннего контроля, инвестиций. Перед Блоком по управлению инновациями стояла задача – выстроить собственную вертикаль в части управления инновациями. В первую очередь речь идет о портфельном подходе к управлению НИОКРами, технологиями, результатами интеллектуальной деятельности, с обеспечением соответствующей экспертизы и инфраструктуры.</p>
-				</div>
-				<div class="b-news-inner clearfix">
-					<h2 class="b-title">Новости</h2>
-					<div class="b-news-list clearfix">
-						<a href="news-detail.php" class="b-news-item">
-							<div class="date">29.6.2017</div>
-							<p>Путин отметил заслуги работников атомной промышленности перед государством</p>
-						</a>
-						<a href="news-detail.php" class="b-news-item">
-							<div class="date">29.6.2017</div>
-							<p>Более 700 специалистов прошли обучение в школах МАГАТЭ по управлению ядерными знаниями</p>
-						</a>
-						<a href="news-detail.php" class="b-news-item">
-							<div class="date">29.6.2017</div>
-							<p>Путин отметил заслуги работников атомной промышленности перед государством</p>
-						</a>
-						<a href="news-detail.php" class="b-news-item">
-							<div class="date">29.6.2017</div>
-=======
-	<div class="b-content-inner-wrap">
-		<ul class="b-nav">
-			<li class="active">
-				<a href="#">Инновационная вертикаль</a>
-				<ul>
-					<li><a href="#">АО «Наука и инновации»</a></li>
-					<li><a href="#">АО «Концерн Росэнергоатом»</a></li>
-					<li><a href="#">АО «АТОМПРОЕКТ»</a></li>
-					<li><a href="#">АО «Атомэнергомаш»</a></li>
-					<li><a href="#">Дивизион ЗСЖЦ (АО ФЦЯРБ)</a></li>
-					<li><a href="#">АО ИК «АСЭ» (АО «НИАЭП»)</a></li>
-					<li><a href="#">Урановый холдинг «АРМЗ»</a></li>
-				</ul>
-			</li>
-			<li><a href="#">Конференции</a></li>
-			<li><a href="#">Семинары/сессии </a></li>
-			<li><a href="#">Конкурсы</a></li>
-			<li><a href="#">Alumni</a></li>
-			<li><a href="#">Экспертиза</a></li>
-			<li><a href="#">Научно-технический совет - ссылка</a></li>
-			<li><a href="#">Тематические группы </a></li>
-		</ul>
-		<div class="b-content-inner">
-			<div class="b-text">
-				<h1>Инновационная вертикаль</h1>
-				<p>Процесс консолидации инновационной деятельности обусловлен задачей достижения технологического лидерства отрасли и подкреплен решением руководства Росатома о построении функциональных вертикалей, которые бы пронизывали все дивизионы. Принцип дивизионального построения корпорации с функциональной вертикалью уже реализован внутри Госкорпорации в сферах управления финансами, кадрами, безопасностью, внутреннего контроля, инвестиций. Перед Блоком по управлению инновациями стояла задача – выстроить собственную вертикаль в части управления инновациями. В первую очередь речь идет о портфельном подходе к управлению НИОКРами, технологиями, результатами интеллектуальной деятельности, с обеспечением соответствующей экспертизы и инфраструктуры.</p>
 
-				<div class="b-news-inner clearfix">
-					<h2>Новости</h2>
-					<div class="b-news-list clearfix">
-						<a href="news-detail.php" class="b-news-item">
-							<div class="date">с 27 сен по 06 окт 2019</div>
-							<p>Путин отметил заслуги работников атомной промышленности перед государством</p>
-						</a>
-						<a href="news-detail.php" class="b-news-item">
-							<div class="date">с 27 сен по 06 окт 2019</div>
-							<p>Более 700 специалистов прошли обучение в школах МАГАТЭ по управлению ядерными знаниями</p>
-						</a>
-						<a href="news-detail.php" class="b-news-item">
-							<div class="date">с 27 сен по 06 окт 2019</div>
-							<p>Путин отметил заслуги работников атомной промышленности перед государством</p>
-						</a>
-						<a href="news-detail.php" class="b-news-item">
-							<div class="date">с 27 сен по 06 окт 2019</div>
->>>>>>> af989089630d5a4894305f9d7ef9b935261df251
-							<p>Более 700 специалистов прошли обучение в школах МАГАТЭ по управлению ядерными знаниями</p>
+					<div class="b-news-inner clearfix">
+						<h2>Новости</h2>
+						<div class="b-news-list clearfix">
+							<a href="news-detail.php" class="b-news-item">
+								<div class="date">с 27 сен по 06 окт 2019</div>
+								<p>Путин отметил заслуги работников атомной промышленности перед государством</p>
+							</a>
+							<a href="news-detail.php" class="b-news-item">
+								<div class="date">с 27 сен по 06 окт 2019</div>
+								<p>Более 700 специалистов прошли обучение в школах МАГАТЭ по управлению ядерными знаниями</p>
+							</a>
+							<a href="news-detail.php" class="b-news-item">
+								<div class="date">с 27 сен по 06 окт 2019</div>
+								<p>Путин отметил заслуги работников атомной промышленности перед государством</p>
+							</a>
+							<a href="news-detail.php" class="b-news-item">
+								<div class="date">с 27 сен по 06 окт 2019</div>
+								<p>Более 700 специалистов прошли обучение в школах МАГАТЭ по управлению ядерными знаниями</p>
+							</a>
+						</div>
+						<a href="news.php" class="b-btn b-btn-tr all-news">
+							<span class="b-btn-content">Все новости</span>
+							<div class="icon-arrow-right"></div>
+							<div class="icon-arrow-right-bold"></div>
 						</a>
 					</div>
-					<a href="news.php" class="b-btn b-btn-tr all-news">
-						<span class="b-btn-content">Все новости</span>
-						<div class="icon-arrow-right"></div>
-						<div class="icon-arrow-right-bold"></div>
-					</a>
+					<div class="b-partners-list clearfix">
+						<a href="#" class="b-partner-item">
+							<img class="b-img" src="i/partner-1.jpg">
+						</a>
+						<a href="#" class="b-partner-item">
+							<img class="b-img" src="i/partner-2.jpg">
+						</a>
+						<a href="#" class="b-partner-item">
+							<img class="b-img" src="i/partner-3.jpg">
+						</a>
+						<a href="#" class="b-partner-item">
+							<img class="b-img" src="i/partner-4.jpg">
+						</a>
+						<a href="#" class="b-partner-item">
+							<img class="b-img" src="i/partner-5.jpg">
+						</a>
+						<a href="#" class="b-partner-item">
+							<img class="b-img" src="i/partner-6.jpg">
+						</a>
+						<a href="#" class="b-partner-item">
+							<img class="b-img" src="i/partner-7.jpg">
+						</a>
+					</div>
 				</div>
-				<div class="b-partners-list clearfix">
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-1.jpg">
-<<<<<<< HEAD
-					</a>
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-2.jpg">
-					</a>
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-3.jpg">
-					</a>
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-4.jpg">
-					</a>
-					<a href="#" class="b-partner-item">
-=======
-					</a>
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-2.jpg">
-					</a>
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-3.jpg">
-					</a>
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-4.jpg">
-					</a>
-					<a href="#" class="b-partner-item">
->>>>>>> af989089630d5a4894305f9d7ef9b935261df251
-						<img class="b-img" src="i/partner-5.jpg">
-					</a>
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-6.jpg">
-					</a>
-					<a href="#" class="b-partner-item">
-						<img class="b-img" src="i/partner-7.jpg">
-					</a>
-				</div>
-<<<<<<< HEAD
-=======
-
->>>>>>> af989089630d5a4894305f9d7ef9b935261df251
 			</div>
 		</div>
 	</div>
 	
 <?include 'footer.php';?>
+		
